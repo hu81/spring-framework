7314e9d1cc92dacb7be61b9211da56b40fde4222
colleen_liu <jackiehu@augmentum.com.cn>
2021-01-14 08:00:03 +0800

    add spring testing module

---
+++ spring-jtesting/src/main/java/com/hu81/Test.java
@@ -0,0 +1,13 @@
+package com.hu81;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User) context.getBean("user");
+		System.out.println(user.toString());
+	}
+
+}
