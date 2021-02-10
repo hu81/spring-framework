4fd2fb474c5c0c74df8198f6834e66d0073b10a6
colleen_liu <jackiehu@augmentum.com.cn>
2021-02-10 15:20:14 +0800

    add docs and testing code

--- spring-jtesting/src/main/java/com/hu81/Test.java
+++ spring-jtesting/src/main/java/com/hu81/Test.java
@@ -2,15 +2,26 @@ package com.hu81;
 
 import com.hu81.controller.WelcomeController;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
 
 @Configuration
 @ComponentScan("com.hu81")
 public class Test {
 
-	public static void main(String[] args) {
+	@Bean("messageSource")
+	ResourceBundleMessageSource resourceBundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames(new String[] { "i18n" });
+		return messageSource;
+	}
+
+	public static void main1(String[] args) {
 		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("C:\\Jackie\\Projects\\Research\\spring\\spring-framework\\spring-jtesting\\src\\main\\resources\\ApplicationContext.xml");
 		for (String beanName : context.getBeanDefinitionNames()) {
 			System.out.println("---------------- " + beanName);
@@ -19,8 +30,11 @@ public class Test {
 		System.out.println(welcomeController.sayHi("81"));
 	}
 
-	public static void main1(String[] args) {
+	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Test.class);
+
+		System.out.println("Spring Default 1: " + context.getMessage("name", null, Locale.getDefault()));
+
 		for (String beanName : context.getBeanDefinitionNames()) {
 			System.out.println("---------------- " + beanName);
 		}
---
+++ spring-jtesting/src/main/java/com/hu81/test/MyBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package com.hu81.test;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+public class MyBeanPostProcessor implements BeanPostProcessor {
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("postProcessBeforeInitialization: " + beanName);
+
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("postProcessAfterInitialization: " + beanName);
+
+		return bean;
+	}
+}
